71a81bffd692af5f5b3ba3f5acceed1a7287b5c7
Vincent Lai <vincentlai91@hotmail.com>
2022-09-11 17:33:31 +0800

    first commit

---
+++ src/main/java/com/trippify/trippifyapp/TrippifyappApplication.java
@@ -0,0 +1,13 @@
+package com.trippify.trippifyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrippifyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrippifyappApplication.class, args);
+	}
+
+}
