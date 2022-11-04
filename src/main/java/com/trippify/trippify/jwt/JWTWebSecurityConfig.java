bf8cc4c65eabb90606b9378efd42c2f59de7bb91
Vincent Lai <vincentlai91@hotmail.com>
2022-11-04 14:54:51 +0800

    enable CORS to allow all POST, GET paths

--- src/main/java/com/trippify/trippify/jwt/JWTWebSecurityConfig.java
+++ src/main/java/com/trippify/trippify/jwt/JWTWebSecurityConfig.java
@@ -66,8 +66,8 @@ public class JWTWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity webSecurity) throws Exception {
-		webSecurity.ignoring().antMatchers(HttpMethod.POST, authenticationPath).antMatchers(HttpMethod.OPTIONS, "/**")
-				.and().ignoring().antMatchers(HttpMethod.GET, "/" // Other Stuff You want to Ignore
+		webSecurity.ignoring().antMatchers(HttpMethod.POST, "/**").antMatchers(HttpMethod.OPTIONS, "/**").and()
+				.ignoring().antMatchers(HttpMethod.GET, "/**" // Other Stuff You want to Ignore
 				).and().ignoring().antMatchers("/h2-console/**/**");// Should not be in Production!
 	}
 }
